539a791f42c470ba64b2650721830eeb35cb52d8
Joe Watson <joe_watson@mail.com>
2016-06-17 08:35:24 +0100

    Tweak regexp to match spec

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ router.get("/", function (req: Express.Request, res: Express.Response): void {
 router.get("/api/whoami", function(req: Express.Request, res: Express.Response): void {
   let whoami: WhoAmI = {
     ipaddress: getClientIp(req),
-    language: beforeFirstComma(req.headers["accept-language"]),
+    language: beforeFirstSeperator(req.headers["accept-language"]),
     software: betweenFirstParens(req.headers["user-agent"])
   };
   res.send(whoami);
@@ -31,12 +31,13 @@ let port: number = process.env.PORT || 8080;
 app.listen(port);
 console.log("Request header server launched on port " + port);
 
-// Helper functions
-function beforeFirstComma(s: string): string {
-    let matches: RegExpExecArray | null = /^[^,]*/.exec(s);
+/** Return the portion of a string upto the first comma or semi-colon */
+function beforeFirstSeperator(s: string): string {
+    let matches: RegExpExecArray | null = /^[^,;]*/.exec(s);
     return matches ? matches[0] : "";
 }
 
+/** Return the contents of the string's first pair of parentheses */
 function betweenFirstParens(s: string): string {
     let matches: RegExpExecArray | null = /\(([^)]*)\)/.exec(s);
     return matches ? matches[1] : "";
